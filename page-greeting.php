9951a99de24eda19eaffb002db863032f2f55ef0
YaoShinnosuke <y-shinnosuke@outlook.com>
2025-01-04 00:17:43 +0900

    院長情報の追加

--- page-greeting.php
+++ page-greeting.php
@@ -50,39 +50,85 @@
 
           </div>
 
-            <!--
-            <div class="p-pageMinorAge__head p-pageMinorHead" style="margin-top: 48px;">
-              <h3 class="p-pageMinorHead__title">経歴</h3>
-            </div>
-            <div class="p-singleRecruitInfo__body">
-              <div class="p-singleRecruitInfo__list">
-                <div class="p-singleRecruitInfo__row">
-                  <dt>20xx.01</dt>
-                  <dd><a href="https://www.med.kanazawa-u.ac.jp/" target="_blank">金沢大学医学部</a> 卒</dd>
+          <div class="p-singleMenu__box"></div>
+          <div class="p-singleMenu__box">
+            <div class="p-singleMenu__flex">
+            
+              <div class="p-singleMenu__flexDesc">
+                
+                <div class="p-pageMinorAge__head p-pageMinorHead" style="margin-top: 48px;">
+                  <h3 class="p-pageMinorHead__title">経歴</h3>
                 </div>
-                <div class="p-singleRecruitInfo__row">
-                  <dt>20xx.01</dt>
-                  <dd><a href="https://www.kouseiren-ta.or.jp/index.html" target="_blank">厚生連高岡病院</a> 勤務</dd>
+                <div class="p-singleRecruitInfo__body">
+                  <div class="p-singleRecruitInfo__list">
+                    <div class="p-singleRecruitInfo__row">
+                      <dt>1992年</dt>
+                      <dd><a href="https://www.aichi-med-u.ac.jp/" target="_blank">愛知医科大学</a> 卒業</dd>
+                    </div>
+                    <div class="p-singleRecruitInfo__row">
+                      <dt>1992年</dt>
+                      <dd><a href="https://www.aichi-med-u.ac.jp/hospital/" target="_blank">愛知医科大学付属病院</a> 第二外科医局 入局 </dd>
+                    </div>
+                    <div class="p-singleRecruitInfo__row">
+                      <dt>1993年</dt>
+                      <dd><a href="http://www.tsushimacity-hp.jp/" target="_blank">津島市民病院</a> 外科</dd>
+                    </div>
+                    <div class="p-singleRecruitInfo__row">
+                      <dt>2003年</dt>
+                      <dd>同病院 外科医長</dd>
+                    </div>
+                    <div class="p-singleRecruitInfo__row">
+                      <dt>2007年</dt>
+                      <dd>同病院 外科副部長</dd>
+                    </div>
+                    <div class="p-singleRecruitInfo__row">
+                      <dt>2008年</dt>
+                      <dd>同病院 救急医療部長</dd>
+                    </div>
+                    <div class="p-singleRecruitInfo__row">
+                      <dt>2022年</dt>
+                      <dd>4月 某医療痩身クリニック入職<br>同病院院長就任</dd>
+                    </div>
+                    <div class="p-singleRecruitInfo__row">
+                      <dt>2024年</dt>
+                      <dd>リエートクリニック名古屋院 院長就任<br>理事長就任</dd>
+                    </div>
+                  </div>
                 </div>
-                <div class="p-singleRecruitInfo__row">
-                  <dt>20xx.01</dt>
-                  <dd><a href="https://web.hosp.kanazawa-u.ac.jp/" target="_blank">金沢大学附属病院</a> 勤務</dd>
+
+              </div>
+              <div class="p-singleMenu__flexDesc">
+                <div class="p-pageMinorAge__head p-pageMinorHead" style="margin-top: 48px;">
+                  <h3 class="p-pageMinorHead__title">資格</h3>
                 </div>
-                <div class="p-singleRecruitInfo__row">
-                  <dt>20xx.01</dt>
-                  <dd>美容クリニック 勤務</dd>
-                </div>
-                <div class="p-singleRecruitInfo__row">
-                  <dt>2024.04</dt>
-                  <dd>リエートクリニック横浜院 就任</dd>
-                </div>
-                <div class="p-singleRecruitInfo__row">
-                  <dt>2024.06</dt>
-                  <dd>リエートクリニック横浜院院長 就任</dd>
+                <div class="p-singleRecruitInfo__body">
+                  <div class="p-singleRecruitInfo__list">
+                    <div class="p-singleRecruitInfo__row">
+                      <dt>1992年</dt>
+                      <dd>医師免許取得</dd>
+                    </div>
+                    <div class="p-singleRecruitInfo__row">
+                      <dt>2002年</dt>
+                      <dd><a href="https://jp.jssoc.or.jp/" target="_blank">日本外科学会</a>外科専門医取得</dd>
+                    </div>
+                    <div class="p-singleRecruitInfo__row">
+                      <dt>2015年</dt>
+                      <dd><a href="https://www.jaam.jp/" target="_blank">日本救急医学会</a>救急科専門医取得</dd>
+                    </div>
+                    <div class="p-singleRecruitInfo__row">
+                      <dd><br class="sp-block"><a href="https://www.jrcmc-eccm.com/" target="_blank">ICLS.JATEC.JPTEC.MCLSインストラクター</a></dd>
+                    </div>
+                    <div class="p-singleRecruitInfo__row">
+                      <dd><br class="sp-block"><a href="https://www.mhlw.go.jp/content/10800000/000478174.pdf" target="_blank">DMAT統括資格.地域災害医療コーデイネーター</a></dd>
+                    </div>
+                  </div>
                 </div>
               </div>
+
             </div>
-            -->
+          </div>
+
+         
 
         </div>
       </section>
