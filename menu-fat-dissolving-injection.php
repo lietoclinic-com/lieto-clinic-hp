7c5ee34c7cfcf445c16ef031ff017d1869baf7ac
YaoShinnosuke <y-shinnosuke@outlook.com>
2025-09-29 13:53:08 +0900

    関連コラムを表示

--- menu-fat-dissolving-injection.php
+++ menu-fat-dissolving-injection.php
@@ -672,6 +672,70 @@
         </div>
       </div>
 
+      <!-- 関連コラム -->
+      <div class="p-singleMenu__area l-inner" id="injection_faq">
+        <h2 class="p-singleMenu__title _nols">関連コラム</h2>
+
+        <div class="p-singleMenu__column">
+
+          <?php
+            $args = array(
+              'post_type' => 'post',
+              'posts_per_page' => 3,
+              'post_status' => 'publish',
+            );
+            $query = new WP_Query($args);
+          ?>
+          <?php if ($query->have_posts()) : ?>
+                <ul class="p-pageColumn__articlesList">
+                  <?php while ($query->have_posts()) : $query->the_post(); ?>
+                    <li class="p-pageColumn__articlesItem">
+                      <a href="<?php the_permalink(); ?>">
+                        <figure class="p-pageColumn__articlesFigure">
+                          <?php if (has_post_thumbnail()) : ?>
+                            <?php the_post_thumbnail(); ?>
+                          <?php else : ?>
+                            <img loading="lazy" src="<?php echo esc_url(get_template_directory_uri()); ?>/dist/img/common/sample_01.png" alt="記事サムネイル">
+                          <?php endif; ?>
+                        </figure>
+
+                        <div class="p-pageColumn__articlesDesc">
+                          <div class="p-pageColumn__articlesCat"><?php echo get_the_terms($post->ID, 'category')[0]->name; ?></div>
+                          <div class="p-pageColumn__dates c-articleDates">
+                            <div class="c-articleDates__modified">
+                              <div class="c-articleDates__modifiedTtl">更新日</div>
+                              <time class="c-articleDates__modifiedNum" datetime="<?php the_modified_time('Y-m-d'); ?>" itemprop="datemodified"><?php the_modified_time('Y.m.d') ?></time>
+                            </div>
+                            <div class="c-articleDates__published">
+                              <div class="c-articleDates__publishedTtl">公開日</div>
+                              <time class="c-articleDates__publishedNum" datetime="<?php the_time('Y-m-d'); ?>" itemprop="datepublished"><?php the_time('Y.m.d'); ?></time>
+                            </div>
+                          </div>
+
+                          <h3 class="p-pageColumn__articlesTitle">
+                            <?php
+                            if (mb_strlen($post->post_title) > 39) {
+                              $title = mb_substr($post->post_title, 0, 39);
+                              echo $title . '...';
+                            } else {
+                              echo $post->post_title;
+                            }
+                            ?>
+                          </h3>
+                        </div>
+                      </a>
+                    </li>
+                  <?php endwhile; ?>
+                </ul>
+          <?php endif; wp_reset_postdata(); ?>
+
+          <div class="p-injectionMerit__btn">
+            <a href="<?php echo esc_url(home_url()); ?>/column/" class="p-injectionMerit__btnLink c-btn">すべてのコラムをみる</a>
+          </div>
+
+        </div>
+      </div>
+
       <!-- CTA -->
       <div class="p-singleMenu__cta l-inner">
         <?php get_template_part('template-parts/part', 'cta'); ?>
