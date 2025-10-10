960c612caa8f0c9c7e7007ea72faac81cc568122
YaoShinnosuke <y-shinnosuke@outlook.com>
2025-10-11 01:39:00 +0900

    インタビュー記事公開

---
+++ page-interview.php
@@ -0,0 +1,353 @@
+<?php get_header(); ?>
+
+<main class="l-main">
+
+  <section class="p-pageMessage">
+
+    <div class="p-pageMessage__body">
+
+      <!-- 担当医師挨拶 -->
+      <section class="p-pageMessageGreeting">
+        <div class="p-pageMessageGreeting__inner l-inner">
+          <figure class="p-pageMessageGreeting__mainimg">
+            <img class="sp-hide" loading="lazy" src="<?php echo esc_url(get_template_directory_uri()); ?>/dist/img/interview01-mv.webp" alt="「我慢しないで痩せられるは本当でした」">
+            <img class="sp-block" loading="lazy" src="<?php echo esc_url(get_template_directory_uri()); ?>/dist/img/interview01-mv-sp.webp" alt="「我慢しないで痩せられるは本当でした」">
+          </figure>
+
+          <h1 class="p-pageMessageGreeting__head is-interview">
+            <p class="p-pageMessageGreeting__title">
+              「我慢しないで痩せられるは本当でした」<br>　66kg→48kg、医療ダイエットの記録
+            </p>
+          </h1>
+
+          <div class="p-pageMessageGreeting__desc">
+            <p class="p-pageMessageGreeting__text">
+              「健康診断の結果が、今までで一番悪かったんです…」そう語るのは、リエートクリニックで医療ダイエットに取り組み、4ヶ月で17キロの減量に成功したY.A様。健康への危機感から始まったダイエットの体験談を詳しくお聞きしました。
+            </p>
+          </div>
+
+        </div>
+      </section>
+
+      <div class="p-singleColumn__content p-content is-interview">
+        
+        <!-- きっかけは最悪の健康診断結果 -->
+        <h2 class="wp-block-heading"><span>きっかけは最悪の健康診断結果</span></h2>
+        <figure class="wp-block-image size-full" style="max-width: 400px; width: 60%; margin-left: auto; margin-right: auto;">
+          <img src="<?php echo esc_url(get_template_directory_uri()); ?>/dist/img/interview01-img01.webp" alt="医療ダイエットを始める前">
+          <figcaption style="text-align: center;">医療ダイエットを始める前</figcaption>
+        </figure>
+
+        <div class="c-balloon c-balloon--left">
+          <figure class="c-balloon__image">
+            <img decoding="async" width="75" height="75" src="<?php echo esc_url(get_template_directory_uri()); ?>/dist/img/interview-icon.webp" class="attachment-thumbnail size-thumbnail" alt="">
+            <figcaption>リエート</figcaption>
+          </figure>
+          <div class="c-balloon__textWrapper c-balloon__textWrapper--left">
+            <div class="c-balloon__text">
+              <p>医療ダイエットを始めようと思ったきっかけを教えてください。</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="c-balloon c-balloon--right">
+          <figure class="c-balloon__image">
+            <img decoding="async" width="75" height="75" src="<?php echo esc_url(get_template_directory_uri()); ?>/dist/img/interview01-icon.webp" class="attachment-thumbnail size-thumbnail" alt="">
+            <figcaption>Y.A様</figcaption>
+          </figure>
+          <div class="c-balloon__textWrapper c-balloon__textWrapper--right">
+            <div class="c-balloon__text">
+              <p>
+                健康診断の結果が、今までで一番悪かったんです。正直ショックで…健康になりたくて始めました。ネットで医療ダイエットのことを知って、これなら効果があるかもしれないと思ったんです。
+                <br>
+                <br>これまでも食事制限や糖質制限など、いろんなダイエットを試してきました。糖質抜きダイエットもやってみたんですが、続かなかったんですよね。どれも我慢が必要で、結局挫折してしまうことが多くて…。
+                <br>
+                <br>医療ダイエットを始める前は、薬の副作用が強いのかなという不安はありました。でも、医療だから安心して受けられると思っていたので、期待の方が大きかったです。
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="c-balloon c-balloon--left">
+          <figure class="c-balloon__image">
+            <img decoding="async" width="75" height="75" src="<?php echo esc_url(get_template_directory_uri()); ?>/dist/img/interview-icon.webp" class="attachment-thumbnail size-thumbnail" alt="">
+            <figcaption>リエート</figcaption>
+          </figure>
+          <div class="c-balloon__textWrapper c-balloon__textWrapper--left">
+            <div class="c-balloon__text">
+              <p>たくさんあるクリニックの中で、リエートクリニックを選んだ理由を教えてください。</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="c-balloon c-balloon--right">
+          <figure class="c-balloon__image">
+            <img decoding="async" width="75" height="75" src="<?php echo esc_url(get_template_directory_uri()); ?>/dist/img/interview01-icon.webp" class="attachment-thumbnail size-thumbnail" alt="">
+            <figcaption>Y.A様</figcaption>
+          </figure>
+          <div class="c-balloon__textWrapper c-balloon__textWrapper--right">
+            <div class="c-balloon__text">
+              <p>
+                全ての機械が医療機器だということが決め手でした。ちゃんとした医療機器を使っているので、安心して効果が得られるだろうなと思って選びました。
+                <br>
+                <br>最初のカウンセリングの時のスタッフの方の励ましもあり、「ここにしよう」って決めました。
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <!-- 医療の力で「我慢しない」ダイエットが実現 -->
+        <h2 class="wp-block-heading"><span>医療の力で「我慢しない」ダイエットが実現</span></h2>
+        <figure class="wp-block-image size-full">
+          <img src="<?php echo esc_url(get_template_directory_uri()); ?>/dist/img/interview01-img02.webp" alt="医療の力で「我慢しない」ダイエットが実現">
+        </figure>
+
+        <div class="c-balloon c-balloon--left">
+          <figure class="c-balloon__image">
+            <img decoding="async" width="75" height="75" src="<?php echo esc_url(get_template_directory_uri()); ?>/dist/img/interview-icon.webp" class="attachment-thumbnail size-thumbnail" alt="">
+            <figcaption>リエート</figcaption>
+          </figure>
+          <div class="c-balloon__textWrapper c-balloon__textWrapper--left">
+            <div class="c-balloon__text">
+              <p>今回受けられた医療ダイエットの内容を詳しく教えてください。</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="c-balloon c-balloon--right">
+          <figure class="c-balloon__image">
+            <img decoding="async" width="75" height="75" src="<?php echo esc_url(get_template_directory_uri()); ?>/dist/img/interview01-icon.webp" class="attachment-thumbnail size-thumbnail" alt="">
+            <figcaption>Y.A様</figcaption>
+          </figure>
+          <div class="c-balloon__textWrapper c-balloon__textWrapper--right">
+            <div class="c-balloon__text">
+              <p>
+                脂肪冷却、ハイフ、電磁場機器、脂肪溶解注射、それからサプリメントやお薬などを組み合わせたプランにしました。施術とお薬を並行する感じでしたね。
+                <br>
+                <br>その中でも、お薬の効果がすごく実感できました。食事の満腹感がとても良くて、少量でも十分な満腹感が得られるんです。薬のおかげで我慢や辛い気持ちが全然なくて、楽しく、我慢せずにダイエットできました。
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="c-balloon c-balloon--left">
+          <figure class="c-balloon__image">
+            <img decoding="async" width="75" height="75" src="<?php echo esc_url(get_template_directory_uri()); ?>/dist/img/interview-icon.webp" class="attachment-thumbnail size-thumbnail" alt="">
+            <figcaption>リエート</figcaption>
+          </figure>
+          <div class="c-balloon__textWrapper c-balloon__textWrapper--left">
+            <div class="c-balloon__text">
+              <p>今までのダイエットと医療ダイエットとで、違いを感じましたか？</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="c-balloon c-balloon--right">
+          <figure class="c-balloon__image">
+            <img decoding="async" width="75" height="75" src="<?php echo esc_url(get_template_directory_uri()); ?>/dist/img/interview01-icon.webp" class="attachment-thumbnail size-thumbnail" alt="">
+            <figcaption>Y.A様</figcaption>
+          </figure>
+          <div class="c-balloon__textWrapper c-balloon__textWrapper--right">
+            <div class="c-balloon__text">
+              <p>
+                全然違いますね。今までのダイエットと比べて、我慢という感覚があまりなくて、本当にラクして楽しくできました。これまでの辛いダイエットとは全然違う体験でした。
+                <br>                
+                <br>唯一苦労したのは、お薬が自己注射だったので、それがちょっと怖くて…初めは少し勇気が入りました（笑）。でも痛みはありませんでした。
+              </p>
+            </div>
+          </div>
+        </div>
+
+
+        <!-- 1週間で1キロ減！驚きの即効性 -->
+        <h2 class="wp-block-heading"><span>1週間で1キロ減！驚きの即効性</span></h2>
+        <figure class="wp-block-image size-full">
+          <img src="<?php echo esc_url(get_template_directory_uri()); ?>/dist/img/interview01-img03.webp" alt="1週間で1キロ減！驚きの即効性">
+        </figure>
+
+        <div class="c-balloon c-balloon--left">
+          <figure class="c-balloon__image">
+            <img decoding="async" width="75" height="75" src="<?php echo esc_url(get_template_directory_uri()); ?>/dist/img/interview-icon.webp" class="attachment-thumbnail size-thumbnail" alt="">
+            <figcaption>リエート</figcaption>
+          </figure>
+          <div class="c-balloon__textWrapper c-balloon__textWrapper--left">
+            <div class="c-balloon__text">
+              <p>効果が出るまでにはどれくらいの期間がかかりましたか？</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="c-balloon c-balloon--right">
+          <figure class="c-balloon__image">
+            <img decoding="async" width="75" height="75" src="<?php echo esc_url(get_template_directory_uri()); ?>/dist/img/interview01-icon.webp" class="attachment-thumbnail size-thumbnail" alt="">
+            <figcaption>Y.A様</figcaption>
+          </figure>
+          <div class="c-balloon__textWrapper c-balloon__textWrapper--right">
+            <div class="c-balloon__text">
+              <p>
+                私は結構早かったです。体重計に乗って数字を見ていたんですが、始めて1、2週間ぐらいで1キロぐらい痩せました。その後も順調に落ちていったので、効果はしっかり実感できました。見た目の変化を感じたのは、1ヶ月ちょっと経ってからですね。
+                <br>
+                <br>ダイエット中の生活も、すごく変わりました。以前は仕事終わりに缶チューハイを買ったり、仕事中にコーヒーをよく飲んでいたんですが、ダイエット中はそれをやめてみたんです。そしたら次の日の目覚めがすごく良くて、「こっちの方がいい！」って実感しました。
+                <br>
+                <br>今は終わった後も、時々飲みに行きますが、そんなにたくさん飲むことはなくなりましたね。
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <!-- スタッフの温かいサポートが支えに -->
+        <h2 class="wp-block-heading"><span>スタッフの温かいサポートが支えに</span></h2>
+
+        <div class="c-balloon c-balloon--left">
+          <figure class="c-balloon__image">
+            <img decoding="async" width="75" height="75" src="<?php echo esc_url(get_template_directory_uri()); ?>/dist/img/interview-icon.webp" class="attachment-thumbnail size-thumbnail" alt="">
+            <figcaption>リエート</figcaption>
+          </figure>
+          <div class="c-balloon__textWrapper c-balloon__textWrapper--left">
+            <div class="c-balloon__text">
+              <p>クリニックの医師やスタッフの印象はいかがでしたか？</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="c-balloon c-balloon--right">
+          <figure class="c-balloon__image">
+            <img decoding="async" width="75" height="75" src="<?php echo esc_url(get_template_directory_uri()); ?>/dist/img/interview01-icon.webp" class="attachment-thumbnail size-thumbnail" alt="">
+            <figcaption>Y.A様</figcaption>
+          </figure>
+          <div class="c-balloon__textWrapper c-balloon__textWrapper--right">
+            <div class="c-balloon__text">
+              <p>
+                皆さんすごく優しくて、自分の話すことをしっかり聞いてくれるんです。
+                <br>ダメって言わないで、全部褒めてくれるのがとても嬉しかったです。ダイエット中って自分に厳しくなりがちですが、スタッフの方々に支えられました。
+                <br>
+                <br>結果的に4ヶ月で17キロ痩せることができました。自分でも信じられないぐらいの結果です。
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <!-- 無理のない体重維持でリバウンドなし -->
+        <h2 class="wp-block-heading"><span>無理のない体重維持でリバウンドなし</span></h2>
+        <figure class="wp-block-image size-full">
+          <img src="<?php echo esc_url(get_template_directory_uri()); ?>/dist/img/interview01-img04.webp" alt="無理のない体重維持でリバウンドなし">
+        </figure>
+
+        <div class="c-balloon c-balloon--left">
+          <figure class="c-balloon__image">
+            <img decoding="async" width="75" height="75" src="<?php echo esc_url(get_template_directory_uri()); ?>/dist/img/interview-icon.webp" class="attachment-thumbnail size-thumbnail" alt="">
+            <figcaption>リエート</figcaption>
+          </figure>
+          <div class="c-balloon__textWrapper c-balloon__textWrapper--left">
+            <div class="c-balloon__text">
+              <p>現在も体重を維持するために心がけていることはありますか？</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="c-balloon c-balloon--right">
+          <figure class="c-balloon__image">
+            <img decoding="async" width="75" height="75" src="<?php echo esc_url(get_template_directory_uri()); ?>/dist/img/interview01-icon.webp" class="attachment-thumbnail size-thumbnail" alt="">
+            <figcaption>Y.A様</figcaption>
+          </figure>
+          <div class="c-balloon__textWrapper c-balloon__textWrapper--right">
+            <div class="c-balloon__text">
+              <p>
+                今は水を毎日2リットルぐらい飲むように心がけています。食事は、カロリー制限というよりは「ここまで」って決めて、厳しくしすぎないようにしています。
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="c-balloon c-balloon--left">
+          <figure class="c-balloon__image">
+            <img decoding="async" width="75" height="75" src="<?php echo esc_url(get_template_directory_uri()); ?>/dist/img/interview-icon.webp" class="attachment-thumbnail size-thumbnail" alt="">
+            <figcaption>リエート</figcaption>
+          </figure>
+          <div class="c-balloon__textWrapper c-balloon__textWrapper--left">
+            <div class="c-balloon__text">
+              <p>最後に、医療ダイエットが気になっている方に向けて、アドバイスがあればお願いします。</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="c-balloon c-balloon--right">
+          <figure class="c-balloon__image">
+            <img decoding="async" width="75" height="75" src="<?php echo esc_url(get_template_directory_uri()); ?>/dist/img/interview01-icon.webp" class="attachment-thumbnail size-thumbnail" alt="">
+            <figcaption>Y.A様</figcaption>
+          </figure>
+          <div class="c-balloon__textWrapper c-balloon__textWrapper--right">
+            <div class="c-balloon__text">
+              <p>
+                ダイエットしたいけど、我慢は嫌だという方には、本当に医療ダイエットをおすすめします。ラクして簡単に痩せられるし、我慢もしなくていい。それに医療だから安心して受けられますし、効果も出やすいです。
+                <br>
+                <br>気になっているのであれば、ぜひ一歩踏み出してみることをおすすめします。
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <h2 class="p-singleMenu__title">治療概要</h2>
+
+        <div class="p-singleMenu__box">
+          <dl class="p-singleMenu__detail">
+            <div class="p-singleMenu__detailRow">
+              <dt>施術内容</dt>
+              <dd>
+                リエートクリニックオリジナルプログラム
+                <br>・医療脂肪冷却機器
+                <br>・医療用HIFU
+                <br>・医療電磁場機器（テスラ）
+                <br>・脂肪溶解注射
+                <br>・ダイエット薬セット
+                <br>・医師による投薬指導
+                <br>・オリジナルプロテイン
+                <br>・医師監修サプリメント
+                <br>・管理栄養士によるパーソナル食事指導
+              </dd>
+            </div>
+            <div class="p-singleMenu__detailRow">
+              <dt>リスク・副作用</dt>
+              <dd>便秘、下痢、筋肉痛、赤み、腫れ、内出血、しこり、低血糖、吐気、嘔吐</dd>
+            </div>
+            <div class="p-singleMenu__detailRow">
+              <dt>期間</dt>
+              <dd>4ヶ月</dd>
+            </div>
+            <div class="p-singleMenu__detailRow">
+              <dt>結果</dt>
+              <dd>
+                ・体重： 66.0kg → 48.8kg（-17.2kg）
+                <br>・体脂肪率： 35.4% → 15.5%（-19.9%）
+                <br>・ウエスト： 89.5cm → 65.5cm（-24.0cm）
+              </dd>
+            </div>
+          </dl>
+        </div>
+        <p></p>
+
+        <div class="interview__in-body">
+          <img src="<?php echo esc_url(get_template_directory_uri()); ?>/dist/img/interview01-img05.webp">
+          <img src="<?php echo esc_url(get_template_directory_uri()); ?>/dist/img/interview01-img06.webp">
+        </div>
+
+        <p>
+          ※効果には個人差があります。詳しくはカウンセリング時にご相談ください。
+          <br>※当院で使用している医療機器、医薬品は保険適用とならず、全て自由診療です。
+          <br>※治療内容には国内で未承認の医療機器、医薬品を含みます。<a href="<?php echo home_url('/menu/'); ?>">詳しい副作用やリスクについては各施術ページからご確認ください。</a>
+        </p>
+
+      </div>
+
+    </div>
+
+    <!-- CTA -->
+    <div class="p-pageMessage__cta l-inner">
+      <?php get_template_part('template-parts/part', 'cta'); ?>
+    </div>
+
+  </section>
+
+</main>
+
+<?php get_footer();
